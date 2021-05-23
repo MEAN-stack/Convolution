b9d24645a387638b19ef7b50628f541c90966d8d
Paul Robertson <paul.robertson@costain.com>
2021-05-23 10:00:11 +0100

    fix borders

--- convolve.hs
+++ convolve.hs
@@ -4,6 +4,10 @@ import System.Environment (getArgs)
 import Data.List
 import qualified Data.ByteString as BS
 
+rounddiv p q = if (r*2) >= q then d+1 else d
+               where d = p `div` q
+                     r = p `mod` q
+
 kernId           = [[0,0,0],[0,1,0],[0,0,0]]
 kernBlur         = [[1,1,1],[1,1,1],[1,1,1]]
 kernGaussianBlur = [[1,2,1],[2,4,2],[1,2,1]]
@@ -11,16 +15,15 @@ kernEdgeDetect   = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
 kernSharpen      = [[0,-1,0],[-1,5,-1],[0,-1,0]]
 kernEmboss       = [[-2,-1,0],[-1,1,1],[0,1,2]]
 
+ksum = sum.map sum
+
 -- image convolution with a given kernel
 -- x is the neighbourhood of a pixel
 -- x has the same size and shape as the kernel
--- 
-convolve kernel x = ((max 0).(min 255)) val
-                    where val = ((`div` weight) . ksum . kmul kernel) x
-                          kmul = zipWith (zipWith (*))
-                          ksum = sum.map sum
-                          w = ksum kernel
-                          weight = if w==0 then 1 else w
+--
+convolve kernel w x = ((max 0).(min 255)) val
+                      where val = ((`rounddiv` w) . ksum . kmul kernel) x
+                            kmul = zipWith (zipWith (*))
 
 -- convert a list into a list of 'neighbourhoods'
 -- i.e. the elements to either side of each element in the original list
@@ -35,22 +38,25 @@ ns z = take 3 z : ns (tail z)
 
 -- split a list into a 2-D list of lists
 -- e.g. split 2 "Hello!" -> ["He","ll","o!"]
--- e.g. split 3 [1,2,3,4,5,6] -> [[1,2,3],[4,5,6]] 
+-- e.g. split 3 [1,2,3,4,5,6] -> [[1,2,3],[4,5,6]]
 --
 split n [] = []
 split n x = take n x : (split n . drop n) x
 
--- create a border of 0's around the edge our image
+-- create a border around the edge of our image
 --
-addBorder xss = replicate l z : yss ++ [replicate l z]
-                where yss = map (\xs -> (z:xs)++[z]) xss
-                      l = (length.head) yss
-                      z = 0::Integer
+addBorder xss = (extend . map extend) xss
+                where extend = extendr.extendl
+                      extendr = reverse.extendl.reverse
+                      extendl (x:xs) = x:x:xs
 
-convolveImage img ker = (map.map) (convolve ker) ws
+convolveImage :: [[Integer]] -> [[Integer]] -> [[Integer]]
+convolveImage img ker = (map.map) (convolve ker weight) ws
                         where ys = ns img
                               zs = (map.map) ns ys
                               ws = map transpose zs
+                              w = ksum ker
+                              weight = if w==0 then 1 else w
 
 -- 32 bits little endian at offset 0x12
 getBitmapHeaderWidth bs = b0+b1*256+b2*256*256+b3*256*256*256
@@ -75,7 +81,7 @@ main = do
     let header = BS.take 1078 contents
     let width = getBitmapHeaderWidth header
     let pixelData = BS.drop 1078 contents
-    let imagePixels = (addBorder . split width . map toInteger) (BS.unpack pixelData)
+    let imagePixels = (addBorder . split width . map toInteger . BS.unpack) pixelData
     let newImage = convolveImage imagePixels (getKernel kern)
     let bytes = BS.append header (BS.pack ((map fromInteger . concat) newImage))
     let outFile = takeWhile (/='.') inFile ++ '_':kern ++ (dropWhile (/='.') inFile)
